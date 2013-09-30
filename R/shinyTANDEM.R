596a10fb58ca8d56c8619916ae764da50e8209e1
Frederic-bioinfo <frederic.bioinfo@gmail.com>
2013-09-30 15:18:27 -0400

    Added the possibility to pass a dataset as a parameter directly to the function call: shinyTANDEM(dataset=my.rTResult.object)

--- R/shinyTANDEM.R
+++ R/shinyTANDEM.R
@@ -1,5 +1,24 @@
-shinyTANDEM <- function(port=8100,...) {
+dataset.bool <- FALSE
+dataset.obj <- NULL
+
+shinyTANDEM <- function(dataset=NULL, port=8100,...) {
+  
+  if( !is.null(dataset)) {
+    if (! "rTResult" %in% class(dataset)) {
+      stop("The dataset object must be a rTANDEM result object of class='rTResult'.")
+    } else {
+      
+      ## Use tempfile to be plateform independant.
+      filename <- tempfile("sessionDataset.Rds")
+      ### But remove the randomized ending so the shinyApp can access the same file.
+      filename <- gsub("sessionDataset.Rds.*", "sessionDataset.Rds", filename)
+      saveRDS(dataset, filename)
+    }
+  }
+  
+  on.exit(unlink(filename), add=TRUE)
   runApp(appDir=system.file("extdata", package="shinyTANDEM"),
-         port=port
-         )
+    port=port
+  )
 }
+
